cf8f75ff0fb6d82d25cde644c1c896d975a1ce91
Bryan Coutts <b2coutts@uwaterloo.ca>
2013-07-20 14:13:56 -0400

    Changed error handling
    Haskell's error is no longer used; instead, Error String is another clause of
    the Value type. All Funcs defined in Init.hs return something of type Error if
    any part of the argument they depend on has type Error; they append their own
    function name, leaving a sort of "stack trace".
    
    In addition, substr and strcon were removed from Init.hs; they will later be
    added in lib/
    
    Also, I added a todo.txt file to make tracking TODOs easier.

--- Dtypes.hs
+++ Dtypes.hs
@@ -18,15 +18,17 @@ data Token = Strtok String | Inttok Int | Dotint Int | Arg Int | Dotarg Int |
 
 -- datatype for evaluated values in an FL program
 data Value = Strval String | Intval Int | Lmbval Int Scope [Token] |
-             Empty | Cons Value Value | Func Int ([Value] -> Value)
+             Empty | Cons Value Value | Func Int ([Value] -> Value) |
+             Error String
 
 instance Show Value where
     show (Strval x)         = show x
     show (Intval x)         = show x
-    show (Lmbval i s t)     = "Lmb" ++ show i ++ show t
+    show (Lmbval i s t)     = "Lmb" ++  show t
     show Empty              = ";"
     show (Cons a b)         = "," ++ show a ++ " " ++ show b
     show (Func i _)         = "Func" ++ show i
+    show (Error e)          = "ERR(" ++ e ++ ")"
 
 
 -- datatype for storing a symbol table and an argument list
--- Init.hs
+++ Init.hs
@@ -15,11 +15,15 @@ import Data.Map as Map
 int2     :: (Int -> Int -> Int) -> Value
 int2 f   = Func 2 (\x -> case x of
     [Intval a, Intval b]  -> Intval $ f a b
-    _                     -> error "ERROR: Arith function needs 2 Ints as args")
+    _                     -> Error "ERROR: Arith function needs 2 Ints as args")
 
 -- helper function for returning booleans
-boolToVal           :: Bool -> Value
-boolToVal x         = if x then Intval 1 else Intval 0
+boolToVal               :: Bool -> Value
+boolToVal x             = if x then Intval 1 else Intval 0
+
+-- helper function for evaluating Intvals as bools
+valToBool               :: Value -> Bool
+valToBool (Intval i)    = i /= 0
 
 initTable = [
     ("+",       int2 (+)),
@@ -28,85 +32,90 @@ initTable = [
     ("/",       int2 quot),
     ("mod",       int2 mod),
 
-    ("not",     Func 1 $ boolToVal . mynot),
+    ("not",     Func 1 $ mynot),
     ("if",      Func 3 myif),
     ("else",    Func 1 (\[x] -> x)),
-    ("gt",      Func 2 $ boolToVal . gt),
-    ("lt",      Func 2 $ boolToVal . lt),
-    ("eq",      Func 2 $ boolToVal . eq),
+    ("gt",      Func 2 $ gt),
+    ("lt",      Func 2 $ lt),
+    ("eq",      Func 2 $ eq),
 
     ("empty",   Empty),
     ("cons",    Func 2 (\[a,b] -> Cons a b)),
     ("car",     Func 1 mycar),
     ("cdr",     Func 1 mycdr),
 
-    ("substr",  Func 3 mysubstr),
-    ("strcat",  Func 2 mystrcat),
-
     ("error",   Func 1 myerror)
     ]
 
-gt :: [Value] -> Bool
-gt [Intval a, Intval b] = a > b
-gt [Strval a, Strval b] = a > b
+gt :: [Value] -> Value
+gt [Error e, _]             = Error $ "gt1: " ++ e
+gt [_, Error e]             = Error $ "gt2: " ++ e
+gt [Intval a, Intval b]     = boolToVal $ a > b
+gt [Strval a, Strval b]     = boolToVal $ a > b
 gt [Cons a b, Cons c d]
-    | gt [a, c]             = True
-    | lt [a, c]             = False
+    | valToBool $ gt [a, c] = boolToVal $ True
+    | valToBool $ lt [a, c] = boolToVal $ False
     | otherwise             = gt [b, d]
-gt [Empty, _]               = False
-gt [_, Empty]               = False
-gt _                        = error "Usage: gt Int Int or gt String String"
+gt [Empty, _]               = boolToVal $ False
+gt [_, Empty]               = boolToVal $ False
+gt _                        = Error "Usage: gt Int Int or gt String String"
 
-lt [Intval a, Intval b] = a < b
-lt [Strval a, Strval b] = a < b
+lt [Error e, _]             = Error $ "lt1: " ++ e
+lt [_, Error e]             = Error $ "lt2: " ++ e
+lt [Intval a, Intval b]     = boolToVal $ a < b
+lt [Strval a, Strval b]     = boolToVal $ a < b
 lt [Cons a b, Cons c d]
-    | lt [a, c]             = True
-    | gt [a, c]             = False
+    | valToBool $ lt [a, c] = boolToVal $ True
+    | valToBool $ gt [a, c] = boolToVal $ False
     | otherwise             = lt [b, d]
-lt _                        = error "Usage: lt Int Int or lt String String"
+lt _                        = Error "Usage: lt Int Int or lt String String"
 
-eq [Intval a, Intval b]     = a == b
-eq [Strval a, Strval b]     = a == b
-eq [Cons a b, Cons c d]     = eq [a,c] && eq [b,d]
-eq [Empty, Empty]           = True
-eq [_, Empty]               = False
-eq [Empty, _]               = False
-eq _                        = error "Usage: eq Val Val"
 
-mycar [Empty]               = error "car called on empty list"
-mycar [Strval ""]           = error "car called on empty string"
+eq [Error e, _, _]          = Error $ "eq1: " ++ e
+eq [_, Error e, _]          = Error $ "eq2: " ++ e
+eq [_, _, Error e]          = Error $ "eq3: " ++ e
+eq [Intval a, Intval b]     = boolToVal $ a == b
+eq [Strval a, Strval b]     = boolToVal $ a == b
+eq [Cons a b, Cons c d]     = boolToVal $ valToBool (eq [a,c])
+                                && valToBool (eq [b,d])
+eq [Empty, Empty]           = boolToVal $ True
+eq [_, Empty]               = boolToVal $ False
+eq [Empty, _]               = boolToVal $ False
+eq _                        = Error "Usage: eq Val Val"
+
+mycar [Error e]             = Error $ "car1: " ++ e
+mycar [Empty]               = Error "car called on empty list"
+mycar [Strval ""]           = Error "car called on empty string"
 mycar [Cons a _]            = a
 mycar [Strval (x:_)]        = Strval [x]
-mycar _                     = error "Usage: car Cons or car String"
+mycar _                     = Error "Usage: car Cons or car String"
 
-mycdr [Empty]               = error "cdr called on empty list"
-mycdr [Strval ""]           = error "cdr called on empty string"
+mycdr [Error e]             = Error $ "cdr1: " ++ e
+mycdr [Empty]               = Error "cdr called on empty list"
+mycdr [Strval ""]           = Error "cdr called on empty string"
 mycdr [Cons _ b]            = b
 mycdr [Strval (_:xs)]       = Strval xs
-mycdr _                     = error "Usage: cdr Cons or cdr String"
+mycdr _                     = Error "Usage: cdr Cons or cdr String"
 
 
-mynot [Intval 0]            = True
-mynot [Strval ""]           = True
-mynot [Empty]               = True
-mynot _                     = False
+mynot [Error e]             = Error $ "not: " ++ e
+mynot [Intval 0]            = boolToVal $ True
+mynot [Strval ""]           = boolToVal $ True
+mynot [Empty]               = boolToVal $ True
+mynot _                     = boolToVal $ False
 
-myif [(Intval 0), _, v]     = v
-myif [(Strval ""), _, v]    = v
+myif [Error e, _, _]        = Error $ "if1: " ++ e
+myif [Intval 0, _, Error e] = Error $ "if3: " ++ e
+myif [Strval "",_, Error e] = Error $ "if3: " ++ e
+myif [Empty, _, Error e]    = Error $ "if3: " ++ e
+myif [Intval 0, _, v]       = v
+myif [Strval "", _, v]      = v
 myif [Empty, _, v]          = v
+myif [_, Error e, _]        = Error $ "if2: " ++ e
 myif [_, v, _]              = v
-myif _                      = error "Usage: if Value Value Value"
+myif _                      = Error "Usage: if Value Value Value"
 
-mysubstr [Strval s, Intval a, Intval b]
-    | length s <= b     = error "ERROR: third arg to substr out of range"
-    | b < a             = error "ERROR: 3rd arg of substr less than 2nd arg"
-    | otherwise         = Strval $ take (b-a) $ drop a s
-mysubstr _                              = error "Usage: substr String Int Int"
-
-mystrcat [Strval a, Strval b]           = Strval $ a ++ b
-mystrcat _                              = error "Usage: strcat String String"
-
-myerror [Strval s]      = error $ "PROGRAM ERROR: " ++ s
-myerror _               = error "ERROR: Usage: error String"
+myerror [Strval s]      = Error $ "PROGRAM ERROR: " ++ s
+myerror _               = Error "ERROR: Usage: error String"
 
 initScope = Scope (Map.fromList initTable) []

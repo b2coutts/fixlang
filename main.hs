48cb83355f6b71ab4d50e9b3a9a7acd98d623430
Bryan Coutts <b2coutts@uwaterloo.ca>
2013-07-20 00:01:33 -0400

    Added library capabilities
    Since Init.hs was really starting to commit heavily to the bloat of the
    interpreter, I decided to allow FL libraries. Init.hs will still define basic,
    essential functions, but FL libraries will define more abstract functions, such
    as foldr.
    
    Currently, you can pass arguments to main.hs specifying libraries you want to
    use, and it will read those files before the current one (i.e., `runhaskell
    main.hs lib/list.fl`). Libraries should generally be a series of let statements
    (and should not evaluate to a value).
    
    TODO: library imports should be done in the .fl file that needs the library,
    not when actually compiling the file.
    
    TODO: Change list output to use FL format instead of Haskell format.
    
    TODO: Add documentation for libraries and !
    
    TODO: Extend libraries, move stuff out of Init.hs
    
    TODO: implement project euler answers

--- main.hs
+++ main.hs
@@ -2,8 +2,17 @@
 import Eval
 import Parse
 
+import System.Environment
+import System.IO
+import Control.Applicative
+
+-- function to take a string of FL code, evaluate it, and print the result
+evalPrint = putStrLn . show . evaluate
+
 main = do
+    args <- getArgs
     input <- getContents
 
-    putStrLn $ show $ evaluate $ input
-    -- putStrLn $ show $ parse $ input
+    libs <- concat <$> mapM readFile args
+
+    evalPrint $ libs ++ input

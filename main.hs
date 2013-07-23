f52cc1e68325727935fa2f928e0b5b370617a78f
Bryan Coutts <b2coutts@uwaterloo.ca>
2013-07-22 22:09:55 -0400

    More sophisticated import path system
    Now, any filename specified in an import that does not start with / or . will
    be relative to the standard library path.

--- main.hs
+++ main.hs
@@ -7,30 +7,31 @@ import System.Environment
 import System.IO
 import Control.Applicative
 
+-- location of FL standard library
+stdLib = "/home/b2coutts/git/fixlang/lib"
+
 -- function that, given a filename, returns a token list for the file
 getTokens                       :: String -> IO [Token]
 getTokens file                  = do
     code <- readFile file
     let (files, tokens) = parse code
 
-    deps <- concat <$> mapM getTokens files
+    deps <- concat <$> mapM (getTokens . findLib) files
 
     return $ deps ++ tokens
+
+-- given a string representing a library, return a filepath
+findLib             :: String -> String
+findLib ""          = error "ERROR: empty library name given for import"
+findLib s@('/':xs)  = s
+findLib s@('.':xs)  = s
+findLib xs          = stdLib ++ "/" ++ xs
 
 -- function to take a list of FL tokens, evaluate it, and print the result
 evalPrint = putStrLn . show . evaluate
 
 main = do
-    {-
-    args <- getArgs
-    input <- getContents
-
-    libs <- concat <$> mapM readFile args
-    evalPrint $ libs ++ input
-    -}
-
     args <- getArgs
     let file = if null args then error "Usage: $0 filename" else head args
     tokens <- getTokens file
-    --putStrLn $ show tokens
     evalPrint tokens
